d022117b2e2b32cc8b926c89145282ac97901f8a
Byron Clark <byron@theclarkfamily.name>
2025-01-04 22:21:52 -0700

    2024 - Day 15 - Part 2

--- src/solutions/2024/day15.clj
+++ src/solutions/2024/day15.clj
@@ -18,7 +18,7 @@
 ;; # Solution
 ;;
 ;; Load and parse our input
-(def tiles {\# :wall \. :floor \O :box \@ :robot})
+(def tiles {\# :wall \. :floor \O :box \@ :robot \[ :box-left \] :box-right})
 (def tile->printable (set/map-invert tiles))
 
 (defn- parse-puzzle [input]
@@ -92,7 +92,8 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"))
         (grid/set-at location-1 tile-2)
         (grid/set-at location-2 tile-1))))
 
-;; We'll see how this holds up, but boxes move the same way as robots so we can use the same code.
+;; We'll see how this holds up, but boxes move the same way as robots so we can use the
+;; same code.
 (defn- do-move
   "If possible, perform move on the tile at position.
 
@@ -180,10 +181,226 @@ v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"))
 
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 ;; ## Part 2
-(defn part-2
-  [input]
-  (println "Part 2"))
 
-;; Which gives our answer for the test input
+;; We'll start by widening the map
+(defn- widen
+  [warehouse]
+  (reduce (fn [widened location]
+            (let [[x y] location
+                  start-x (* 2 x)
+                  tiles (case (grid/at warehouse location)
+                          :floor [:floor :floor]
+                          :wall [:wall :wall]
+                          :robot [:robot :floor]
+                          :box [:box-left :box-right])]
+              (-> widened
+                  (grid/set-at [start-x y] (first tiles))
+                  (grid/set-at [(inc start-x) y] (second tiles)))))
+          (grid/create (* 2 (:width warehouse)) (:height warehouse) :floor)
+          (grid/all-locations warehouse)))
+
+
+;; And now for the tricky part, moving boxes vertically. We have to figure out the
+;; boundaries of the box and then check that both sides can move in the requested
+;; direction, recursively pushing them around.
+(defn- do-move-box-vertically
+  [warehouse position move]
+  {:pre [(#{:up :down} move)]}
+  (bc/cond
+    :let [tile (grid/at warehouse position)
+          target-position (next-position position move)
+          [x y] position
+          [left right] (if (= :box-left tile)
+                         [position [(inc x) y]]
+                         [[(dec x) y] position])
+          target-left (next-position left move)
+          target-right (next-position right move)
+          target-left-tile (grid/at warehouse target-left)
+          target-right-tile (grid/at warehouse target-right)
+          move-box (fn [next-warehouse]
+                     [(-> next-warehouse
+                          (swap-tiles left target-left)
+                          (swap-tiles right target-right))
+                      target-position])]
+
+    (or (= :wall target-left-tile) (= :wall target-right-tile))
+    [warehouse position]
+
+    (and (= :floor target-left-tile) (= :floor target-right-tile))
+    (move-box warehouse)
+
+    ;; Box sitting completely on top
+    (and (= :box-left target-left-tile) (= :box-right target-right-tile))
+    (let [[after-attempt after-attempt-position] (do-move-box-vertically warehouse target-position move)]
+      (if (= after-attempt-position target-position)
+        [warehouse position]
+        (move-box after-attempt)))
+
+    ;; Box sitting on only the right side
+    (= :floor target-left-tile)
+    (let [[after-attempt after-attempt-position] (do-move-box-vertically warehouse target-right move)]
+      (if (= after-attempt-position target-right)
+        [warehouse position]
+        (move-box after-attempt)))
+
+    ;; Box sitting only on the left side
+    (= :floor target-right-tile)
+    (let [[after-attempt after-attempt-position] (do-move-box-vertically warehouse target-left move)]
+      (if (= after-attempt-position target-left)
+        [warehouse position]
+        (move-box after-attempt)))
+
+    ;; Two boxes sitting on top, need to ensure both can move
+    :let [[after-left-warehouse after-left-position]
+          (do-move-box-vertically warehouse target-left move)]
+
+    ;; Left side couldn't move
+    (= after-left-position target-left)
+    [warehouse position]
+
+    :let [[after-right-warehouse after-right-position]
+          (do-move-box-vertically after-left-warehouse target-right move)]
+
+    ;; Right side couldn't move
+    (= after-right-position target-right)
+    [warehouse position]
+
+    :else
+    (move-box after-right-warehouse)))
+
+(defn- do-move-double-wide
+  "If possible, perform move on the tile at position.
+
+  Returns the updated warehouse and new position of the tile."
+  [warehouse position move]
+  (bc/cond
+    :let [target-position (next-position position move)
+          target-tile (grid/at warehouse target-position)]
+
+    (= :wall target-tile)
+    [warehouse position]
+
+    (= :floor target-tile)
+    (let [next-warehouse (swap-tiles warehouse position target-position)]
+      [next-warehouse target-position])
+
+    ;; Now we start moving boxes
+    ;; Left and right are easy since we can treat either side of the box as a box.
+    (#{:left :right} move)
+    (let [[after-attempt target-after-attempt]
+          (do-move-double-wide warehouse target-position move)]
+      (if (= target-after-attempt target-position)
+        [warehouse position]
+        [(swap-tiles after-attempt position target-position) target-position]))
+
+    :let [[after-attempt target-after-attempt]
+          (do-move-box-vertically warehouse target-position move)]
+
+    ;; No move possible
+    (= target-after-attempt target-position)
+    [warehouse position]
+
+    :else
+    [(swap-tiles after-attempt position target-position) target-position]))
+
+;; ### Time for some experiments
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+;; Here's what the tiny puzzle looks like in double wide mode
+(str/split-lines (grid/display (widen (:warehouse tiny-puzzle)) tile->printable))
+
+;; #### Nothing in the way
+(let [warehouse (widen (:warehouse tiny-puzzle))
+      [next-warehouse next-position] (do-move-double-wide warehouse (robot-position warehouse) :up)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; #### Wall in the way
+(let [warehouse (widen (:warehouse tiny-puzzle))
+      [next-warehouse next-position] (do-move-double-wide warehouse (robot-position warehouse) :left)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; #### Box in the way, no space to move box
+(let [warehouse (widen (:warehouse tiny-puzzle))
+      start-position [6 2]
+      setup-warehouse (swap-tiles warehouse start-position (robot-position warehouse))
+      [next-warehouse next-position] (do-move-double-wide setup-warehouse start-position :up)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; #### Box in the way horizontally, empty space before wall
+(let [warehouse (widen (:warehouse tiny-puzzle))
+      start-position [7 2]
+      setup-warehouse (swap-tiles warehouse start-position (robot-position warehouse))
+      [next-warehouse next-position] (do-move-double-wide setup-warehouse start-position :right)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; #### Multiple boxes in the way vertically, empty space before wall
+(let [warehouse (widen (:warehouse tiny-puzzle))
+      start-position [8 6]
+      setup-warehouse (swap-tiles warehouse start-position (robot-position warehouse))
+      [next-warehouse next-position] (do-move-double-wide setup-warehouse start-position :up)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; #### And then some boxes stacked oddly
+(let [warehouse (grid/character-lines->grid "#########
+#.......#
+#.[][]..#
+#..[]...#
+#...@...#
+#########" :convert tiles)
+      [next-warehouse next-position]
+      (do-move-double-wide warehouse (robot-position warehouse) :up)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; #### And then some boxes stacked oddly that can't move
+(let [warehouse (grid/character-lines->grid "#########
+#....[].#
+#.[][]..#
+#..[]...#
+#...@...#
+#########" :convert tiles)
+      [next-warehouse next-position]
+      (do-move-double-wide warehouse (robot-position warehouse) :up)]
+  {:position next-position
+   :warehouse (str/split-lines (grid/display next-warehouse tile->printable))})
+
+;; ### And now that moving works, we can go back to the solution
+{:nextjournal.clerk/visibility {:code :show :result :hide}}
+(defn part-2
+  [{:keys [warehouse moves]}]
+  (let [widened (widen warehouse)
+        [after-moves _] (reduce (fn [[warehouse position] move]
+                                  (do-move-double-wide warehouse position move))
+                                [widened (robot-position widened)]
+                                moves)]
+    (->> after-moves
+           grid/all-locations
+           (filter #(= :box-left (grid/at after-moves %)))
+           (map gps-coordinate)
+           (apply +))))
+
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+;; We also have a new tiny puzzle for part 2
+(def part-2-tiny-puzzle (parse-puzzle "#######
+#...#.#
+#.....#
+#..OO@#
+#..O..#
+#.....#
+#######
+
+<vv<<^^<<^^"))
+
+;; Which gives our answer for the tiny test input
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
-(part-2 tiny-puzzle)
+(part-2 part-2-tiny-puzzle)
+
+;; And the medium test input
+(part-2 test-puzzle)
+
+;; And for the full input
+(part-2 puzzle)
