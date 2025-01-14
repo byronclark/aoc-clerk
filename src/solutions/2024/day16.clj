57f0779f9620503afb97d48ed1cdddf701a39330
Byron Clark <byron@theclarkfamily.name>
2025-01-13 20:14:22 -0700

    2024 - Day 16 - Part 2

--- src/solutions/2024/day16.clj
+++ src/solutions/2024/day16.clj
@@ -2,12 +2,12 @@
 (ns solutions.2024.day16
   {:nextjournal.clerk/toc true}
   (:require
+   [better-cond.core :as bc]
    [clojure.java.io :as io]
-   [util :as u]
+   [clojure.set :as set]
    [nextjournal.clerk :as clerk]
-   [clojure.string :as str]
    [solutions.2024.helpers.grid :as grid]
-   [better-cond.core :as bc]))
+   [util :as u]))
 
 ;; # Problem
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
@@ -42,6 +42,25 @@
 #.###.#.#.#.#.#
 #S..#.....#...#
 ###############" :convert tiles))
+
+;; And a medium sized test
+(def medium-input (grid/character-lines->grid "#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################" :convert tiles))
 
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 ;; ## Part 1
@@ -99,7 +118,7 @@
 ;; coat of paint.
 (defn a*
   [graph start-location {:keys [target-fn cost-fn done?-fn]}]
-  (let [MAX-VALUE (Long/MAX_VALUE)]
+  (let [MAX-VALUE Long/MAX_VALUE]
     (loop [open-set #{start-location}
            g-scores {start-location 0}]
       (bc/cond
@@ -139,15 +158,85 @@
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
 (part-1 test-input)
 
+;; And the medium input
+(part-1 medium-input)
+
 ;; And the full input
 (part-1 input)
 
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 ;; ## Part 2
-(defn part-2
-  [input]
-  (println "Part 2"))
+;;
+;; It's almost like this part was designed to make using A* difficult because we now want
+;; *all* the paths with the same cost. But I think we can make this work.
 
-;; Which gives our answer
+;; We'll start with A* but we need to store the path that got to each $g(n)$. Unlike our
+;; original implementation we'll need to handle equal cost differently. Instead of
+;; replacing the paths to that point, we'll collect the new paths and keep the existing
+;; ones.
+(defn a*-with-path
+  [graph start-location {:keys [target-fn cost-fn done?-fn]}]
+  (let [MAX-VALUE Long/MAX_VALUE]
+    (loop [open-set #{start-location}
+           paths {start-location #{[start-location]}}
+           g-scores {start-location 0}]
+      (bc/cond
+        (empty? open-set) ;no path available
+        nil
+
+        :let [current (apply min-key #(get g-scores % MAX-VALUE) open-set)
+              current-g-score (get g-scores current)
+              current-paths (get paths current)]
+        (done?-fn current)
+        current-paths
+
+        :let [[open-set' paths' g-scores']
+              (reduce (fn [[open-set paths g-scores] connection]
+                        (let [target (target-fn connection)
+                              target-g-score (get g-scores target)
+                              test-g-score (+ current-g-score (cost-fn connection))
+                              target-paths (set (map #(conj % target) current-paths))]
+                          (cond
+                            ;; Better path than we've found so far
+                            (or (nil? target-g-score)
+                                (< test-g-score target-g-score))
+                            [(conj open-set target)
+                             (assoc paths target target-paths)
+                             (assoc g-scores target test-g-score)]
+
+                            ;; Equivalent path
+                            (= test-g-score target-g-score)
+                            [(conj open-set target)
+                             (update paths target (fnil set/union #{}) target-paths)
+                             (assoc g-scores target test-g-score)]
+
+                            :else
+                            [open-set paths g-scores])))
+                      [(disj open-set current) paths g-scores]
+                      (get graph current))]
+        :else
+        (recur open-set' paths' g-scores')))))
+
+(defn part-2
+  [maze]
+  (let [graph (->graph maze)
+        start-location {:point (first (grid/find-value maze :start))
+                        :direction :right}
+        target-location (first (grid/find-value maze :end))
+        paths (a*-with-path graph start-location {:target-fn #(select-keys % [:direction :point])
+                                                  :cost-fn :cost
+                                                  :done?-fn #(= (:point %) target-location)})]
+    (->> paths
+         (mapcat (fn [path] (map :point path)))
+         set
+         count)))
+
+;; Which gives our answer with the small test input
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
+(part-2 test-input)
+
+;; And with the medium test input
+(part-2 medium-input)
+
+;; And the full input
 (part-2 input)
