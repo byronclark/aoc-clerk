296889b073a6ba33d7f726e50436e6aa43842c58
Byron Clark <byron@theclarkfamily.name>
2025-10-27 20:58:07 -0600

    2024 - Day 24 - Part 2

--- src/solutions/2024/day24.clj
+++ src/solutions/2024/day24.clj
@@ -3,9 +3,10 @@
   {:nextjournal.clerk/toc true}
   (:require
    [clojure.java.io :as io]
-   [util :as u]
+   [clojure.set :as set]
+   [clojure.string :as str]
    [nextjournal.clerk :as clerk]
-   [clojure.string :as str]))
+   [util :as u]))
 
 ;; # Problem
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
@@ -134,15 +135,25 @@ tnw OR pbm -> gnj"))
                                        (apply (operation gate-ops)))))))]))
        (into {})))
 
-(defn part-1
-  [circuit]
-  (let [simulator (->simulator circuit)
-        result-nodes (reverse (sort (filter #(str/starts-with? % "z") (keys simulator))))]
+(defn- nodes->value
+  [simulator prefix]
+  (let [result-nodes (reverse (sort (filter #(str/starts-with? % prefix) (keys simulator))))]
     (->> result-nodes
          (map (fn [node] ((get simulator node) simulator)))
          (map #(if @% 1 0))
          (reduce (fn [acc bit]
                    (bit-or (bit-shift-left acc 1) bit))))))
+
+(defn simulate
+  [circuit]
+  (let [simulator (->simulator circuit)]
+    {:x (nodes->value simulator "x")
+     :y (nodes->value simulator "y")
+     :z (nodes->value simulator "z")}))
+
+(defn part-1
+  [circuit]
+  (:z (simulate circuit)))
 
 ;; Which gives our answer with the mini test input
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
@@ -156,10 +167,206 @@ tnw OR pbm -> gnj"))
 
 {:nextjournal.clerk/visibility {:code :show :result :hide}}
 ;; ## Part 2
-(defn part-2
-  [input]
-  (println "Part 2"))
+;;
+;; Well this is *much* more fun.
+;;
+;; We have different test input for this part of the problem.
+;; *WARNING*: the test input is for a circuit that performs bit-and instead of addition.
+(def part-two-small-input (parse-input "x00: 0
+x01: 1
+x02: 0
+x03: 1
+x04: 0
+x05: 1
+y00: 0
+y01: 0
+y02: 1
+y03: 1
+y04: 0
+y05: 1
 
-;; Which gives our answer
+x00 AND y00 -> z05
+x01 AND y01 -> z02
+x02 AND y02 -> z01
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z00"))
+
+;; Time to start with the naive approach. Figure out which output bits are wrong
+;; and see how many wires lead to those bits. The trick here is probably that we
+;; don't really care about the inputs we have as they may not expose all swapped
+;; connections. The simplest case is trying to add all 1s with all 0s to see
+;; what comes out as a zero.
+(defn- register-bit-count
+  [circuit prefix]
+  (count (filter #(str/starts-with? % prefix) (keys circuit))))
+
+(defn- replace-input
+  [circuit prefix value]
+  (reduce (fn [circuit n]
+            (assoc circuit (format "%s%02d" prefix n)
+                   {:type :input
+                    :value (= 1 (bit-and 0x01 (bit-shift-right value n)))}))
+          circuit
+          (range (register-bit-count circuit prefix))))
+
+(defn- all-ones
+  [bits]
+  (reduce (fn [acc _]
+            (bit-or 0x01 (bit-shift-left acc 1)))
+          0
+          (range bits)))
+
+(defn- bit-at
+  [x n]
+  (bit-and 0x01 (bit-shift-right x n)))
+
+(defn- incorrect-result-bits-for-inputs
+  [circuit input-bit-count x y expected]
+  (let [test-circuit (-> circuit
+                         (replace-input "x" x)
+                         (replace-input "y" y))
+        result (:z (simulate test-circuit))]
+    (filter (fn [n] (not= (bit-at expected n) (bit-at result n)))
+            (range input-bit-count))))
+
+;; We'll test a variety of values to try to determine outputs that could possibly be wrong.
+;; - provided inputs
+;; - x = all ones, y = all zeros
+;; - x = all ones, y = single one in each position
+(defn- incorrect-result-bits
+  [expected-fn circuit]
+  (let [input-bit-count (max (register-bit-count circuit "x")
+                             (register-bit-count circuit "y"))
+        {x-initial :x y-initial :y} (simulate circuit)
+        x-input (all-ones input-bit-count)
+        y-inputs (->> (range input-bit-count)
+                      (map #(bit-shift-left 1 %))
+                      (cons 0))
+        test-cases (->> y-inputs
+                        (map (fn [y] [x-input y (expected-fn x-input y)]))
+                        (cons [x-initial y-initial (expected-fn x-initial y-initial)]))]
+    (reduce (fn [incorrect [x y expected]]
+              (set/union incorrect
+                         (set (incorrect-result-bits-for-inputs circuit input-bit-count x y expected))))
+            #{}
+            test-cases)))
+
+;; Once we know the incorrect positions, we should see how many wires lead to
+;; each of those positions to determine if we can reasonably brute force this or
+;; not.
+;;
+;; Beware, this is making the assumption that the gates that need to be swapped
+;; feed at least one of the wrong outputs.
+(defn- nodes-to-output
+  [circuit output-node]
+  (letfn [(inner [visited current]
+            (let [{node-type :type :as node-info} (get circuit current)
+                  next-visited (conj visited current)]
+              (if (= :input node-type)
+                next-visited
+                (->> (:inputs node-info)
+                     (map #(inner next-visited %))
+                     (apply set/union)))))]
+    (inner #{} output-node)))
+
+;; It turns out this doesn't help much. It trims 10 possible nodes from the full
+;; problem set and 0 possible nodes from the test input.
+
+;; It almost sounds like we need to brute force this. Which is terrifying.
+;; Rightfully terrify because it's 2145 combinations to try *just* for the
+;; smaller test input.
+;;
+;; But we can trim quite a bit of the space. Pairs that feed the same gate
+;; *don't* matter because switching them will do nothing. Except not really
+;; because a wire can be connected as an input on many gates.
+;;
+;; We should see how bad that is...
+(defn- destinations-per-wire
+  [circuit]
+  (reduce (fn [wires node-info]
+            (if (= :input (:type node-info))
+              wires
+              (reduce (fn [wires input]
+                        (update wires input (fnil inc 0)))
+                      wires
+                      (:inputs node-info))))
+          {}
+          (vals circuit)))
+
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+(->> part-two-small-input
+     destinations-per-wire
+     (sort-by second)
+     reverse
+     (take 10))
+
+(->> input
+     destinations-per-wire
+     (sort-by second)
+     reverse
+     (take 10))
+
+;; So the full input has *quite a few* nodes that serve as input for two gates.
+;; But maybe we can still skip enough to get away with this.
+
+;; *HOLD ON*, the full input is trying to build a full adder. This is probably a
+;; case where it may be easier to visualize it and then look for the broken
+;; connections.
+{:nextjournal.clerk/visibility {:code :show :result :hide}}
+(defn ->dot
+  [circuit]
+  (let [input-bit-count (max (register-bit-count circuit "x")
+                             (register-bit-count circuit "y"))
+        input-nodes (mapcat (fn [n] [(format "x%02d" n)
+                                     (format "y%02d" n)])
+                            (reverse (range input-bit-count)))
+
+        [nodes wires]
+        (reduce (fn [[nodes wires] [node node-info]]
+                  (let [inputs (:inputs node-info)
+                        color (case (:operation node-info)
+                                :xor "red"
+                                :and "green"
+                                :or "yellow")]
+                    [(conj nodes (format "%s [style=filled,fillcolor=%s]" node color))
+                     (concat wires
+                               [(str (first inputs) " -> " node)
+                                (str (second inputs) " -> " node)])]))
+                [[] []]
+                (reverse (sort (remove #(let [[node _] %]
+                                          (or (str/starts-with? node "x")
+                                              (str/starts-with? node "y")))
+                                       circuit))))]
+    (str/join "\n" (concat ["digraph C {"]
+                           input-nodes
+                           nodes
+                           [""]
+                           wires
+                           ["}"]))))
+
+(defn part-2
+  [circuit filename]
+  (spit filename (->dot circuit)))
+
+;; Write the full input to a dot file so we can investigate.
+(part-2 input "resources/2024-day24-part2-full.dot")
+
 {:nextjournal.clerk/visibility {:code :hide :result :show}}
-(part-2 input)
+(clerk/caption "Original Circuit"
+               (clerk/image "resources/2024-day24-part2-full.png"))
+
+;; From visual inspection and review, we find:
+{:nextjournal.clerk/visibility {:code :show :result :show}}
+(def swapped #{"z06" "ksv"
+               "nbd" "kbs"
+               "tqq" "z20"
+               "z39" "ckb"})
+
+(->> swapped
+     sort
+     (str/join ","))
+
+{:nextjournal.clerk/visibility {:code :hide :result :show}}
+(clerk/caption "With misplaced wires swapped"
+               (clerk/image "resources/2024-day24-part2-full-repaired.png"))
